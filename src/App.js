ff8c7b83e87eaced6cb0885be6a8d9c1f8ef3b8a
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2020-11-12 00:20:18 +0100

    7.8: ultimate hooks

---
+++ src/App.js
@@ -0,0 +1,88 @@
+  
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const useField = type => {
+  const [value, setValue] = useState('')
+
+  const onChange = event => {
+    event
+      ? setValue(event.target.value)
+      : setValue('')
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useResource = baseUrl => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await axios(baseUrl)
+        setResources(result.data)
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  }, [baseUrl])
+
+  const create = async resource => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+  }
+
+  const service = {
+    create
+  }
+
+  return [resources, service]
+}
+
+const App = () => {
+  const content = useField('text')
+  const name = useField('text')
+  const number = useField('text')
+
+  const [notes, noteService] = useResource('http://localhost:3005/notes')
+  const [persons, personService] = useResource('http://localhost:3005/persons')
+
+  const handleNoteSubmit = event => {
+    event.preventDefault()
+    noteService.create({ content: content.value })
+    content.onChange()
+  }
+  
+  const handlePersonSubmit = (event) => {
+    event.preventDefault()
+    personService.create({ name: name.value, number: number.value})
+    content.onChange()
+  }
+
+  return (
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content} />
+        <button>create</button>
+      </form>
+      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name} /> <br/>
+        number <input {...number} />
+        <button>create</button>
+      </form>
+      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+    </div>
+  )
+}
+
+export default App
